19ae92bd9e8afbbf14929eb2374ec2d513aa9926
Matt Choi <choim3@spu.edu>
2020-11-13 22:47:07 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -5,7 +5,7 @@ def find_item_by_name_in_collection(name, collection)
   # Consult README for inputs and outputs
   
   
-  collection.each do |item|
+  collection.each do |item, y|
       binding.pry
   
 end
