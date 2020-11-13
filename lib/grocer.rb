720275907220b6baa8550f3f83b4fb7857572e11
Matt Choi <choim3@spu.edu>
2020-11-13 22:06:05 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -7,7 +7,7 @@ def find_item_by_name_in_collection(name, collection)
   collection.each do |item|
     binding.pry
     if item == name
-      return item
+      return collection[1]
     else 
       return nil 
     end
